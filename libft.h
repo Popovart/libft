98956e7ec304c3e5add84f846242fe093c89930c
Dmitrii <gmdmintry@gmail.com>
2024-09-19 08:49:53 +0200

    header file init

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmitrii <dmitrii@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 08:00:27 by dmitrii           #+#    #+#             */
+/*   Updated: 2024/09/19 08:42:00 by dmitrii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_isalpha(int c);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(const char *str);
+void    *ft_memset(void *s, int c, size_t n);
+
+#endif
