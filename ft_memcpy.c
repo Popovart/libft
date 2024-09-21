1142dd846c5b9f4d82b70b4fc8bb2e3f02a8073e
Dmitrii <gmdmintry@gmail.com>
2024-09-21 18:30:56 +0200

    next 4 functions

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmitrii <dmitrii@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 17:20:56 by dmitrii           #+#    #+#             */
+/*   Updated: 2024/09/21 18:17:19 by dmitrii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmitrii <dmitrii@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 17:26:06 by dmitrii           #+#    #+#             */
+/*   Updated: 2024/09/21 18:19:32 by dmitrii          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// size_t is unsigned type,when it becomes
+// less than zero it sets to size_t max value
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d == s)
+		return (dest);
+	if (d < s)
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+		return (dest);
+	}
+	i = n;
+	while (i > 0)
+	{
+		d[i - 1] = s[i - 1];
+		i--;
+	}
+	return (dest);
+}
